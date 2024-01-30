e43703c953ab2b24dbc5cdf19ebbe8d8d33439bd
claudioGenz <claudiolanda@genz.com.py>
2024-01-30 11:46:39 -0300

    new data source

--- src/controller/funcionario.controller.ts
+++ src/controller/funcionario.controller.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from "express";
 import { Funcionario } from "../entities/funcionario.entitie";
+import { AppDataSource } from "../database/db";
+
+const repo = AppDataSource.getRepository(Funcionario);
+
 
 export const getFuncionarios = async (req: Request, res: Response) => {
   try {
-    const funcionarios = await Funcionario.find();
-    res.status(200).json(funcionarios);
+    const funcionarios = await repo.find({ where: { estado: true } });
+    return res.status(200).json(funcionarios);
   } catch (error) {
     console.log(error);
     if (error instanceof Error) {
-      res.status(500).json({ message: error.message })
+      return res.status(500).json({ message: error.message })
     }
   }
 };
